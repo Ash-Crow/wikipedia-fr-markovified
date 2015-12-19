717efe2a1c61e6b5d50d6596a9d5cb285287d14e
Sylvain Boissel <sylvain@ashtree.eu>
2015-12-19 18:39:08 +0100

    Style

--- parse_content.py
+++ parse_content.py
@@ -21,9 +21,11 @@ except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import urlopen
 
-def remove_signatures(message):
+def remove_signatures(user, message):
     """Tries to remove the user signature from the messages. Not always possible because of the customized signatures"""
-    signature = "[—-]{0,2}(&nbsp;| )?(<(span|div) .*>)?\[\[.* à \d{1,2}:\d{1,2} \(CES?T\)"
+    signature = "[—-]{0,2}(&nbsp;| )?(<(span|div) .*>)?\[\[(Discussion utilisat(eur|rice)|Utilisat(eur|rice)|User):"
+    signature += user
+    signature +=".* à \d{1,2}:\d{1,2} \(CES?T\)"
     message = re.sub(signature, ' ', message)
     return message
 
@@ -49,24 +51,39 @@ def parse_result(url, next_batch = ""):
                         soup = BeautifulSoup(rev['diff']['*'])
                         newlines = soup.select('.diff-addedline')
                         for n in newlines:
+                            
                             text = n.text.lstrip(':').strip()
 
-                            if user not in users_sentences.keys():
-                                users_sentences[user] = []
-                            if text not in users_sentences[user]:
-                                users_sentences[user].append(text)
+                            if not titles.match(text):
+                                text = remove_signatures(user, text)
+
+                                # remove most html tags
+                                text = re.sub('<[^<]+?>', '', text)
+
+                                if "{{non signé|" in text:
+                                    text =""
+
+                                if text == '@':
+                                    text = ''
+
+                                if user not in users_sentences.keys():
+                                    users_sentences[user] = []
+                                if len(text) and text not in users_sentences[user]:
+                                    users_sentences[user].append(text)
+                                
+                            else:
+                                section_titles.append(text)
 
     if "continue" in data.keys():
         print("continue tag: " + data['continue']['rvcontinue'])
         parse_result(url, data['continue']['rvcontinue'])
 
 def save_text(user, text):
-    text = remove_signatures(text)
-
-    user_file = users_dir + user.replace(' ', '_') + '.txt'
-    with open(user_file,"a") as f:
-        f.write(text + ' ')
-        f.close()
+    if len(text):
+        user_file = users_dir + user.replace(' ', '_') + '.txt'
+        with open(user_file,"a") as f:
+            f.write(text + '\n')
+            f.close()
 
 def authorized_user(user):
     """Check if we want to register this user's edits or not."""
@@ -92,26 +109,40 @@ else:
     base = datetime.datetime.today()
 
 if len(sys.argv) > 2:
-    backlog = sys.argv[2]
+    backlog = int(sys.argv[2])
 else:
     backlog = 30 # the number of days of "Bistro" page to parse from today
 
+# variables
 users_dir = 'users/'
+titles = re.compile('={1,6} ?.* ?={1,6}')
+
 
 root_url = "https://fr.wikipedia.org"
+api_url = root_url + "/w/api.php?"
 dates_list = [format_date(base - datetime.timedelta(days=x), locale='fr_FR', format='long') for x in range(0, backlog)]
 
 for date in dates_list:
     users_sentences = {}
+    section_titles = []
 
     print("{} ".format(date))
-    page_titles = { "titles" : "Wikipédia:Le Bistro/{}".format(date) }
+    params = {
+        "action" : "query",
+        "prop" : "revisions",
+        "format" : "json",
+        "rvprop" : "user",
+        "rvlimit" : 1,
+        "rvdiffto" : "prev",
+        "titles" : "Wikipédia:Le Bistro/{}".format(date) }
     
-    api_call =  root_url + "/w/api.php?action=query&prop=revisions&format=json&rvprop=user&rvlimit=1&rvdiffto=prev&" + urllib.parse.urlencode(page_titles)
+    api_call =  api_url + urllib.parse.urlencode(params)
     # Parameter rvlimit set to 1 because of bug https://phabricator.wikimedia.org/T31223
 
     parse_result(api_call)
 
     for user, chunks in users_sentences.items():
-        text = " ".join(chunks)
+        text = "\n".join(chunks)
         save_text(user, text)
+
+    save_text('section_titles', '\n'.join(section_titles))
